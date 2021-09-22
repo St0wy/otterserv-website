3fe49afa31281c98adb65a2b38da531216ebf35c
Maxime Galliot <galliot.maxime@me.com>
2021-09-22 15:14:56 +0200

    + Ready for the data !

--- js/index.js
+++ js/index.js
@@ -4,43 +4,43 @@ var game = new Vue({
         players: true,
         origins: false,
         classes: false,
-        users:{
-            exylo:{
-                origin : "apian",
-                class : "lumberjack",
-                data:{}
+        users: [{
+                name:"Exylo",
+                origin: "apian",
+                class: "lumberjack",
+                data: {}
+            }, {
+                name:"Stowy_",
+                origin: "wolf",
+                class: "blacksmith",
+                data: {}
+            }, {
+                name:"MoiLePseudo",
+                origin: "avian",
+                class: "fisherman",
+                data: {}
+            }, {
+                name:"Roxaasora",
+                origin: "elytrian",
+                class: "cook",
+                data: {}
+            }, {
+                name:"Swordyn",
+                origin: "enderian",
+                class: "merchant",
+                data: {}
+            }, {
+                name:"Krakao76",
+                origin: "merling",
+                class: "fisherman",
+                data: {}
+            }, {
+                name:"Gougou",
+                origin: "wolf",
+                class: "miner",
+                data: {}
             },
-            stowy:{
-                origin : "wolf",
-                class : "blacksmith",
-                data:{}
-            },
-            moilepseudo:{
-                origin : "avian",
-                class : "fisherman",
-                data:{}
-            },
-            roxaasora:{
-                origin : "elytrian",
-                class : "cook",
-                data:{}
-            },
-            swordyn:{
-                origin : "enderian",
-                class : "merchant",
-                data:{}
-            },
-            krakao:{
-                origin : "merling",
-                class : "fisherman",
-                data:{}
-            },
-            gougou:{
-                origin : "wolf",
-                class : "miner",
-                data:{}
-            },
-        }
+        ]
     },
     methods: {
 
