5f7383e9b5e7d4962a7378ce95c74504f8d9f742
Maxime Galliot <galliot.maxime@me.com>
2021-09-22 15:01:24 +0200

    + follow-up of data optimisation

--- js/index.js
+++ js/index.js
@@ -3,7 +3,44 @@ var game = new Vue({
     data: {
         players: true,
         origins: false,
-        classes: false
+        classes: false,
+        users:{
+            exylo:{
+                origin : "apian",
+                class : "lumberjack",
+                data:{}
+            },
+            stowy:{
+                origin : "wolf",
+                class : "blacksmith",
+                data:{}
+            },
+            moilepseudo:{
+                origin : "avian",
+                class : "fisherman",
+                data:{}
+            },
+            roxaasora:{
+                origin : "elytrian",
+                class : "cook",
+                data:{}
+            },
+            swordyn:{
+                origin : "enderian",
+                class : "merchant",
+                data:{}
+            },
+            krakao:{
+                origin : "merling",
+                class : "fisherman",
+                data:{}
+            },
+            gougou:{
+                origin : "wolf",
+                class : "miner",
+                data:{}
+            },
+        }
     },
     methods: {
 
